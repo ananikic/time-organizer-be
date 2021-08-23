7fd1dc7d1d7571acf96d6aebf0e74ccc6cf70f31
andrijana.nikic <andrijana.nikic@accedia.com>
2021-08-23 16:08:20 +0300

    Initial Commit

---
+++ src/main/java/com/tu/timeorganizerbe/TimeOrganizerBeApplication.java
@@ -0,0 +1,13 @@
+package com.tu.timeorganizerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOrganizerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeOrganizerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tu/timeorganizerbe/TimeOrganizerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tu.timeorganizerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeOrganizerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
